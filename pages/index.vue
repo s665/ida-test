4ffa65dcb57186cd0a41f996cb509227d452f3f5
Stepan Skaryakin <s6658895@gmail.com>
2021-01-21 18:58:09 +0500

    header layout

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h1>Каталог</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  max-width: 1260px;
+}
+</style>
